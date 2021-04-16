866a70d65ee5de74ac9801126813f41e3d919cbf
khaiars <khaiars107@gmail.com>
2021-04-17 02:52:37 +0700

    sap xong css

--- index.php
+++ index.php
@@ -3,12 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
     <title>Joker</title>
+    <link rel="stylesheet" href="./css/mystyle.css" >
+    <link rel=" stylesheet " href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    <sc src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></sc>
+
 </head>
 
 <body>
-<p>aaaa</p>
+    <div class="container">
+            <div class="item item1">
+                <img src="logo.png" ></img> 
+            </div>
+            <div class="item item2"></div>
+            <div class="item item3" >
+                <a > Handcrafted by </a> </br>
+                <a > HL solutions </a>
+            </div>
+            <div class="item item4">
+                <img src="pro.png" alt="">
+            </div>
+    </div>
     
+    <section class="banner">
+        <div class="textBx">
+            <h1>A joker day keeps the doctor way</h1>
+            <h2> if you joke wrong away, you teeth have to pay(Serious)</h2>
+          
+        </div>
+    </section>
+    <section class='main'>
+        <div class='content'>
+            <div class="contentBx ">
+                <p>A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."
+The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
+The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."
+                </p>
+                <hr>
+            </div>
+            <div id="outer">
+            <div class="inner">
+                   <button type="button" class="btn btn-primary btn-lg">This is funny</button>
+
+            </div>  
+            <div class="inner">
+                <button type="button" class="btn btn-secondary btn-lg">This is not funny</button>
+                </div>
+                </div>
+        </div>
+        
+    </section>
+    
+  
 </body>
+
 </html>
