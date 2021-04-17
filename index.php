df82bb0cd4318be3d21dd72749619d0a27d9627f
khaiars <khaiars107@gmail.com>
2021-04-17 18:13:06 +0700

    bb

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
                 <img src="pro.png" alt="">
             </div>
     </div>
-<<<<<<< HEAD
     
     <section class="banner">
         <div class="textBx">
@@ -62,44 +61,9 @@ The child ran back to his father and said, "You lied to me!" His father replied,
         
     </div>
     
-=======
-    
-    <section class="banner">
-        <div class="textBx">
-            <h1>A joker day keeps the doctor way</h1>
-            <h2> if you joke wrong away, you teeth have to pay(Serious)</h2>
-          
-        </div>
-    </section>
-    <section class='main'>
-        <div class='content'>
-            <div class="contentBx ">
-                <p>A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."
-The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
-The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."
-                </p>
-                <hr class="new1">
-            </div>
-            <div id="outer">
-            <div class="inner">
-                   <button type="button" class="btn btn-primary btn-lg">This is funny</button>
 
-            </div>  
-            <div class="inner">
-                <button type="button" class="btn btn-secondary btn-lg">This is not funny</button>
-                </div>
-                </div>
-        </div>
-    </section>
-    <hr class="new2">
-    <div class="footer">
-        <div class="intro"><p> child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on." The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now." The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."</p></div>
-        <div class="copy"><h4>Copyright 2021 HLSolutions Pte.Ltd</h4></div>
-        
-        
-    </div>
     
->>>>>>> ea4910da64b44fb7538142a4ecadcdd8e09ae199
+    
   
 </body>
 
